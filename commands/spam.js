23f854889a6eb22e6b9368ffe01df6b3e1d6cfe6
Koen-mods <koen.veentjer@gmail.com>
2025-06-23 16:45:31 +0200

    Create spam.js

---
+++ commands/spam.js
@@ -0,0 +1,21 @@
+export const data = {
+  name: 'spam',
+  description: 'Spam een prachtige zin in de chat xD',
+};
+
+export async function execute(interaction) {
+  const allowedRoleId = ''; // Vervang dit door de juiste rol ID
+
+  const allowedRoles = ['1353284416620859462', '1386718540358221844']; // Replace with your actual role IDs
+
+  const hasRole = allowedRoles.some(roleId => interaction.member.roles.cache.has(roleId));
+
+  if (!hasRole) {
+  return interaction.reply({ content: 'Je hebt niet de juiste rol om dit commando te gebruiken.', ephemeral: true });
+  }
+
+  // Hier komt de logica van je commando
+  for (let i = 0; i < 1000; i++){
+    await interaction.reply('Help let me go!');
+  }
+}

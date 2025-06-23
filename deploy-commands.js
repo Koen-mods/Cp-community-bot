af8064e7fb8d92fb6663902221873f48136aaf06
Koen-mods <koen.veentjer@gmail.com>
2025-06-23 17:45:01 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -15,6 +15,16 @@ const commands = [
     .setName('spam')
     .setDescription('Spam een prachtige zin in de chat xD')
     .toJSON(),
+  new SlashCommandBuilder()
+  .setName('verbale waarschuwing')
+  .setDescription('Geef iemand een verbale waarschuwing')
+  .addStringOption(option =>
+    option
+      .setName('reden')
+      .setDescription('Reden voor waarschuwing')
+      .setRequired(true)
+  )
+  .toJSON()
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);

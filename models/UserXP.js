71d37a940e49bc2a64ad12461533a388da52adaf
Koen-mods <koen.veentjer@gmail.com>
2025-06-23 15:49:29 +0200

    Create UserXP.js

---
+++ models/UserXP.js
@@ -0,0 +1,10 @@
+// models/UserXP.js
+import mongoose from 'mongoose'
+
+const userXPSchema = new mongoose.Schema({
+  userId: { type: String, required: true, unique: true },
+  xp: { type: Number, default: 0 },
+  level: { type: Number, default: 1 }
+});
+
+module.exports = mongoose.model('UserXP', userXPSchema);
